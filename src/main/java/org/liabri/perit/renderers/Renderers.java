6972d31c34a9155ac6d5095289e2c88f77fb7797
liabri <liabri@tuta.io>
2022-03-11 11:22:43 +0100

    initial commit with basic structure

---
+++ src/main/java/org/liabri/perit/renderers/Renderers.java
@@ -0,0 +1,13 @@
+package org.liabri.perit.renderers;
+
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import org.liabri.perit.blocks.Blocks;
+
+public class Renderers {
+    public static void init() {}
+
+    static {
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STONE.getLeft(), RenderLayer.getCutout());
+    }
+}
