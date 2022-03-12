05ebb85971d80b89bf2c8c1b2d09e4758da96463
lb06 <lbcat@tutanota.com>
2022-03-12 11:12:43 +0100

    add packed terracotta

--- src/main/java/org/liabri/perit/materials/Materials.java
+++ src/main/java/org/liabri/perit/materials/Materials.java
@@ -8,19 +8,19 @@ public class Materials {
 
     public static void init() {}
 
-    public static final ColouredMaterial[] colouredMaterials = new ColouredMaterial[2];
+    public static final ColouredMaterial[] colouredMaterials = new ColouredMaterial[3];
     public static final StoneMaterial[] stoneMaterials = new StoneMaterial[1];
 
 
     static {
         colouredMaterials[0] = new ColouredMaterial("terracotta_bricks", "terracotta_brick", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
         colouredMaterials[1] = new ColouredMaterial("terracotta_shingles", "terracotta_shingle", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
-//        colouredMaterials[2] = new ColouredMaterial("terracotta", Blocks.TERRACOTTA, true);
+        colouredMaterials[2] = new ColouredMaterial("packed_terracotta", "packed_terracotta", FabricBlockSettings.copy(Blocks.TERRACOTTA), false);
 
 
         stoneMaterials[0] = new StoneMaterial("andesite", FabricBlockSettings.copyOf(Blocks.ANDESITE), ItemGroup.BUILDING_BLOCKS, new StoneMaterial.Kinds[]{
                 StoneMaterial.Kinds.Base,
-                StoneMaterial.Kinds.Smooth,
+                StoneMaterial.Kinds.Polished,
         });
     }
 }
