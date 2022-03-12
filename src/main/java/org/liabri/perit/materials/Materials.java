3d7f1cb39374c064eeda66ae057739040a5835a6
lb06 <lbcat@tutanota.com>
2022-03-12 12:36:37 +0100

    remove coloured material and convert to a simple list

--- src/main/java/org/liabri/perit/materials/Materials.java
+++ src/main/java/org/liabri/perit/materials/Materials.java
@@ -8,14 +8,43 @@ public class Materials {
 
     public static void init() {}
 
-    public static final ColouredMaterial[] colouredMaterials = new ColouredMaterial[3];
+    public static final String[] COLOURS = {
+            "black",
+            "blue",
+            "brown",
+            "cyan",
+            "green",
+            "grey",
+            "light_blue",
+            "light_grey",
+            "lime",
+            "magenta",
+            "orange",
+            "pink",
+            "purple",
+            "red",
+            "white",
+            "yellow"
+    };
+
     public static final StoneMaterial[] stoneMaterials = new StoneMaterial[1];
 
-
     static {
-        colouredMaterials[0] = new ColouredMaterial("terracotta_bricks", "terracotta_brick", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
-        colouredMaterials[1] = new ColouredMaterial("terracotta_shingles", "terracotta_shingle", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
-        colouredMaterials[2] = new ColouredMaterial("packed_terracotta", "packed_terracotta", FabricBlockSettings.copy(Blocks.TERRACOTTA), false);
+        for (String colour: COLOURS) {
+            stoneMaterials[0] = new StoneMaterial(colour, "terracotta", FabricBlockSettings.copyOf(Blocks.TERRACOTTA), ItemGroup.BUILDING_BLOCKS, new StoneMaterial.Kinds[]{
+                    StoneMaterial.Kinds.Base,
+                    StoneMaterial.Kinds.Smooth,
+                    StoneMaterial.Kinds.Cut,
+                    StoneMaterial.Kinds.CrackedCut,
+                    StoneMaterial.Kinds.Polished,
+                    StoneMaterial.Kinds.CrackedPolished,
+                    StoneMaterial.Kinds.Cobbled,
+                    StoneMaterial.Kinds.MossyCobbled
+            });
+        }
+//        colouredMaterials[0] = new ColouredMaterial("terracotta_bricks", "terracotta_brick", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
+//        colouredMaterials[1] = new ColouredMaterial("terracotta_shingles", "terracotta_shingle", FabricBlockSettings.copy(Blocks.TERRACOTTA), true);
+//        colouredMaterials[2] = new ColouredMaterial("packed_terracotta", "packed_terracotta", FabricBlockSettings.copy(Blocks.TERRACOTTA), false);
 
 
 //        stoneMaterials[0] = new StoneMaterial("andesite", FabricBlockSettings.copyOf(Blocks.ANDESITE), ItemGroup.BUILDING_BLOCKS, new StoneMaterial.Kinds[]{
