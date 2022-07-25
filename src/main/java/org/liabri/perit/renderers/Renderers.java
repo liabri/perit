436c859d6b615c4f66218bcaef677264ce945fa4
liabri <liabri@tuta.io>
2022-07-26 01:24:26 +0200

    janky client render

--- src/main/java/org/liabri/perit/renderers/Renderers.java
+++ src/main/java/org/liabri/perit/renderers/Renderers.java
@@ -1,9 +1,22 @@
 package org.liabri.perit.renderers;
 
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.Block;
+import net.minecraft.client.render.RenderLayer;
+import org.liabri.perit.blocks.Blocks;
+
+import java.util.Objects;
+
+import static org.liabri.perit.Perit.id;
+
 public class Renderers {
     public static void init() {}
 
     static {
-//        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.WARPED_NETHER_WART.getLeft(), RenderLayer.getCutout());
+        for (Block block : Blocks.blocks) {
+            if (Objects.equals(block.getLootTableId().getPath(), "blocks/stripped_bamboo_door")) {
+                BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
+            }
+        }
     }
 }
